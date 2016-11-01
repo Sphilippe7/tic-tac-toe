82eb2d4d6267d19420bc6441f6ab2d84d7847788
sphilippe7 <solomon924@gmail.com>
2016-11-01 18:02:38 -0400

    My first Deploy
    Made changes and got some of the ground work done

--- scripts/sign-in.sh
+++ scripts/sign-in.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-#curl "http://localhost:3000/sign-in" \
-curl "http://httpbin.org/post" \
-  --include \
+#API="http://httpbin.org"
+#URL_PATH="/post"
+API="http://localhost:4741"
+URL_PATH="/sign-in"
+curl "${API}${URL_PATH}" \
   --request POST \
-  --data-urlencode ""
+  --data-urlencode "credentials[email]=${EMAIL}" \
+  --data-urlencode "credentials[password]=${PASSWORD}"
 
 echo
